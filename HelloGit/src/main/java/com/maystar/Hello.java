56a0cf028934c19c92118c092f555c6f6c5e49e8
zf <zf7655@163.com>
2021-06-13 12:45:04 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/maystar/Hello.java
@@ -0,0 +1,12 @@
+package com.maystar;
+
+/**
+ * @Description
+ * @auther admin
+ * @create 2021-06-13 12:33
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
